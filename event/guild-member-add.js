446ec117ff89a39ae08e177231a04988f1e079c6
spiral <spiral@spiral.sh>
2022-02-01 13:27:25 -0500

    re-enable guild member add handler..

--- event/guild-member-add.js
+++ event/guild-member-add.js
@@ -2,6 +2,6 @@ const Guild = require('../model/guild');
 
 module.exports = async (member) => {
     if ((member.guild === null || isRightGuild(member.guild.id)) && !member.user.bot) {
-        // Guild.guildMemberAddHandler(member);
+        Guild.guildMemberAddHandler(member);
     }
 };
